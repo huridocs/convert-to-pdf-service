ec42cea3f395a9b5304bb49a59b43d64000b7cc2
Daneryl <daneryl@gmail.com>
2022-11-17 10:15:41 +0100

    custom network

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -19,28 +19,38 @@ class EndToEnd(unittest.TestCase):
             requests.post(f"{service_url}/upload/{namespace}", files=files)
 
         queue = RedisSMQ(host="127.0.0.1", port="6379", qname="convert-to-pdf_tasks")
+        # task = json.loads('{"task": "convert-to-pdf", "params": {"filename": file-sample_1MB.docx, "namespace": documents}}')
 
         queue.sendMessage().message(
-            '{"task": "convert-to-pdf", "params": {"filename": file-sample_1MB.docx, "namespace": documents}}'
+            '{"task": "convert-to-pdf", "params": {"filename": "file-sample_1MB.docx", "namespace": "documents"}}'
         ).execute()
 
-        self.get_redis_message()
+        message = self.get_redis_message()
+        print(message);
 
-        file = requests.get(f"{service_url}/processed_pdf/document/file-sample_1MB.pdf")
+        response = requests.get(f"{service_url}/processed_pdf/documents/file-sample_1MB.pdf")
+
+        # response = requests.get(message['file_url'])
+        self.assertEqual(200, response.status_code)
+        # self.assertEqual(task.params, message.params)
+
+        # with pdfplumber.open(io.BytesIO(response.content)) as pdf:
+        #     first_page = pdf.pages[0]
+        #     return first_page.extract_text()
 
         self.assertIsNotNone(file)
 
 
     @staticmethod
     def get_redis_message():
-        queue = RedisSMQ(host="127.0.0.1", port="6379", qname="convert-to-pdf_results", quiet=True)
+        queue = RedisSMQ(host="127.0.0.1", port="6379", qname="convert-to-pdf_results")
 
         for i in range(50):
             time.sleep(0.5)
             message = queue.receiveMessage().exceptions(False).execute()
             if message:
                 queue.deleteMessage(id=message["id"]).execute()
-                return True
+                return json.loads(message["message"])
 
 
 
