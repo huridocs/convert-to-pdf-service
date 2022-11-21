c4cfd22e766ba10ab925954cfe4e44e4c77b5674
Daneryl <daneryl@gmail.com>
2022-11-21 10:43:09 +0100

    clean up

--- src/create_task.py
+++ src/create_task.py
@@ -1,5 +1,8 @@
 from rsmq import RedisSMQ
+import os
 
-queue = RedisSMQ(host='localhost', port=6379, qname='convert-to-pdf_tasks', quiet=True)
+host = os.environ.get('REDIS_HOST', 'localhost')
+
+queue = RedisSMQ(host=host, port=6379, qname='convert-to-pdf_tasks', quiet=True)
 message_json = '{"tenant": "new-namespace", "task": "convert-to-pdf", "params": {"filename": "form.png", "namespace": "document"}}'
 queue.sendMessage().message(message_json).execute()
