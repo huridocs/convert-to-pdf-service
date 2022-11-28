892357c9ff0b8768020ef220a7058b2a17d7be6a
Sergio Revilla <sergio.revilla@hey.com>
2022-11-28 12:30:02 +0100

    Do not ignore config files

---
+++ src/worker/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+APP_PATH = Path(__file__).parent.absolute()
+DATA_PATH = f"{APP_PATH}/../data"
+DOCUMENT_SOURCES_PATH = f"{DATA_PATH}/source_documents"
+PDF_PROCESSED_PATH = f"{DATA_PATH}/processed_pdfs"
+DOCUMENT_FAILED = f"{DATA_PATH}/failed_documents"
+
+CONFIG = {
+    "source_documents": DOCUMENT_SOURCES_PATH,
+    "processed_pdfs": PDF_PROCESSED_PATH,
+    "failed_documents": DOCUMENT_FAILED,
+}
