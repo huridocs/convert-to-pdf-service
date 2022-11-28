6715767e6073be7d194fa4cce25eb7dc5cc4bdf9
Sergio Revilla <sergio.revilla@hey.com>
2022-11-28 13:26:44 +0100

    Remove service config (#25)
    * Remove ServiceConfig from API
    
    * Remove from worker
    
    * Do not ignore config files
    
    * Configure logging
    
    * Remove log to file

--- src/worker/convert_to_pdf.py
+++ src/worker/convert_to_pdf.py
@@ -3,9 +3,7 @@ import shutil
 import pathlib
 import subprocess
 
-from ServiceConfig import ServiceConfig
-
-config = ServiceConfig()
+from config import CONFIG
 
 THIS_SCRIPT_PATH = pathlib.Path(__file__).parent.absolute()
 DATA_PATH = f"{THIS_SCRIPT_PATH}/../data"
@@ -13,10 +11,10 @@ DATA_PATH = f"{THIS_SCRIPT_PATH}/../data"
 
 def get_paths(namespace: str, pdf_file_name: str):
     file_name = "".join(pdf_file_name.split(".")[:-1])
-    source_document_filepath = f'{config.paths["source_documents"]}/{namespace}/{pdf_file_name}'
-    processed_pdf_dir = f'{config.paths["processed_pdfs"]}/{namespace}'
+    source_document_filepath = f'{CONFIG["source_documents"]}/{namespace}/{pdf_file_name}'
+    processed_pdf_dir = f'{CONFIG["processed_pdfs"]}/{namespace}'
     processed_pdf_filepath = f'{processed_pdf_dir}/{file_name}.pdf'
-    failed_documents_filepath = f'{config.paths["failed_documents"]}/{namespace}/{pdf_file_name}'
+    failed_documents_filepath = f'{CONFIG["failed_documents"]}/{namespace}/{pdf_file_name}'
     return source_document_filepath, processed_pdf_dir, processed_pdf_filepath, failed_documents_filepath
 
 
@@ -42,7 +40,7 @@ def convert_to_pdf(filename, namespace):
         os.remove(source_document_filepath)
         return processed_pdf_filepath
 
-    if not os.path.exists(f'{config.paths["failed_documents"]}/{namespace}'):
-        os.makedirs(f'{config.paths["failed_documents"]}/{namespace}', exist_ok=True)
+    if not os.path.exists(f'{CONFIG["failed_documents"]}/{namespace}'):
+        os.makedirs(f'{CONFIG["failed_documents"]}/{namespace}', exist_ok=True)
     shutil.move(source_document_filepath, failed_documents_filepath)
     return False
