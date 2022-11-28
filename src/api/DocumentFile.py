b38b7d94f0afc5e8319ae5bd00cb926f7c2f9d36
Sergio Revilla <sergio.revilla@hey.com>
2022-11-28 11:01:50 +0100

    Remove ServiceConfig from API

--- src/api/DocumentFile.py
+++ src/api/DocumentFile.py
@@ -1,9 +1,20 @@
 import os
 import pathlib
 from pathlib import Path
-from ServiceConfig import ServiceConfig
 
-config = ServiceConfig()
+APP_PATH = Path(__file__).parent.absolute()
+DATA_PATH = f"{APP_PATH}/../data"
+DOCUMENT_SOURCES_PATH = f"{DATA_PATH}/source_documents"
+PDF_PROCESSED_PATH = f"{DATA_PATH}/processed_pdfs"
+DOCUMENT_FAILED = f"{DATA_PATH}/failed_documents"
+
+CONFIG = {
+    "app": APP_PATH,
+    "data": DATA_PATH,
+    "source_documents": DOCUMENT_SOURCES_PATH,
+    "processed_pdfs": PDF_PROCESSED_PATH,
+    "failed_documents": DOCUMENT_FAILED,
+}
 
 
 class DocumentFile:
@@ -13,15 +24,15 @@ class DocumentFile:
         self.namespace = namespace
 
     def save(self, document_file_name: str, file: bytes):
-        sourceFolder = "source_documents"
+        source_folder = CONFIG["source_documents"]
 
-        if not os.path.exists(config.paths[sourceFolder]):
-            os.mkdir(config.paths[sourceFolder])
+        if not os.path.exists(CONFIG[source_folder]):
+            os.mkdir(CONFIG[source_folder])
 
-        if not os.path.exists(f'{config.paths[sourceFolder]}/{self.namespace}'):
-            os.mkdir(f'{config.paths[sourceFolder]}/{self.namespace}')
+        if not os.path.exists(f'{CONFIG[source_folder]}/{self.namespace}'):
+            os.mkdir(f'{CONFIG[source_folder]}/{self.namespace}')
 
-        path = f'{config.paths[sourceFolder]}/{self.namespace}/{document_file_name}'
+        path = f'{CONFIG[source_folder]}/{self.namespace}/{document_file_name}'
 
         file_path_document = pathlib.Path(path)
         file_path_document.write_bytes(file)
