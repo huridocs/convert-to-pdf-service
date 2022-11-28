7377b1e3d94df2923ec220a0a706d422646ae82d
Joan Gallego Girona <daneryl@gmail.com>
2022-11-28 13:51:00 +0000

    add black formatter as dev dependency (#26)
    * add black formatter as dev dependency
    
    * formatted the whole app with black

--- src/worker/convert_to_pdf.py
+++ src/worker/convert_to_pdf.py
@@ -11,18 +11,30 @@ DATA_PATH = f"{THIS_SCRIPT_PATH}/../data"
 
 def get_paths(namespace: str, pdf_file_name: str):
     file_name = "".join(pdf_file_name.split(".")[:-1])
-    source_document_filepath = f'{CONFIG["source_documents"]}/{namespace}/{pdf_file_name}'
+    source_document_filepath = (
+        f'{CONFIG["source_documents"]}/{namespace}/{pdf_file_name}'
+    )
     processed_pdf_dir = f'{CONFIG["processed_pdfs"]}/{namespace}'
-    processed_pdf_filepath = f'{processed_pdf_dir}/{file_name}.pdf'
-    failed_documents_filepath = f'{CONFIG["failed_documents"]}/{namespace}/{pdf_file_name}'
-    return source_document_filepath, processed_pdf_dir, processed_pdf_filepath, failed_documents_filepath
+    processed_pdf_filepath = f"{processed_pdf_dir}/{file_name}.pdf"
+    failed_documents_filepath = (
+        f'{CONFIG["failed_documents"]}/{namespace}/{pdf_file_name}'
+    )
+    return (
+        source_document_filepath,
+        processed_pdf_dir,
+        processed_pdf_filepath,
+        failed_documents_filepath,
+    )
 
 
 def convert_to_pdf(filename, namespace):
-    source_document_filepath, processed_pdf_dir, processed_pdf_filepath, failed_documents_filepath = get_paths(
-        namespace, filename
-    )
-    os.makedirs(f'/{processed_pdf_dir}', exist_ok=True)
+    (
+        source_document_filepath,
+        processed_pdf_dir,
+        processed_pdf_filepath,
+        failed_documents_filepath,
+    ) = get_paths(namespace, filename)
+    os.makedirs(f"/{processed_pdf_dir}", exist_ok=True)
 
     result = subprocess.run(
         [
@@ -32,7 +44,7 @@ def convert_to_pdf(filename, namespace):
             "pdf",
             "--outdir",
             processed_pdf_dir,
-            source_document_filepath
+            source_document_filepath,
         ]
     )
 
