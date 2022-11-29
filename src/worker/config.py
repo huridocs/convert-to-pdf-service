090e0d30f110d2a5aba7bec25fad52dba0b0fb84
Sergio Revilla <sergio.revilla@hey.com>
2022-11-29 13:10:21 +0100

    Docker tuning

--- src/worker/config.py
+++ src/worker/config.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 
 APP_PATH = Path(__file__).parent.absolute()
-DATA_PATH = f"{APP_PATH}/../data"
+DATA_PATH = f"{APP_PATH}/data"
 DOCUMENT_SOURCES_PATH = f"{DATA_PATH}/source_documents"
 PDF_PROCESSED_PATH = f"{DATA_PATH}/processed_pdfs"
 DOCUMENT_FAILED = f"{DATA_PATH}/failed_documents"
