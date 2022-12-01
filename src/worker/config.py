bc27099dddd70310be128d944b7e93c6039d8557
Joan Gallego Girona <daneryl@gmail.com>
2022-12-01 09:32:58 +0000

    graylog_logging support (#38)

--- src/worker/config.py
+++ src/worker/config.py
@@ -1,4 +1,6 @@
 import logging
+import graypy
+import os
 from pathlib import Path
 
 APP_PATH = Path(__file__).parent.absolute()
@@ -6,6 +8,7 @@ DATA_PATH = f"{APP_PATH}/../data"
 DOCUMENT_SOURCES_PATH = f"{DATA_PATH}/source_documents"
 PDF_PROCESSED_PATH = f"{DATA_PATH}/processed_pdfs"
 DOCUMENT_FAILED = f"{DATA_PATH}/failed_documents"
+GRAYLOG_IP = os.environ.get('GRAYLOG_IP')
 
 CONFIG = {
     "source_documents": DOCUMENT_SOURCES_PATH,
@@ -13,10 +16,16 @@ CONFIG = {
     "failed_documents": DOCUMENT_FAILED,
 }
 
+logging.root.handlers = []
+handlers = [logging.StreamHandler()]
+if GRAYLOG_IP:
+    handlers.append(graypy.GELFUDPHandler(
+        GRAYLOG_IP, 12201, localname="convert-to-pdf"
+    ))
 
 logging.root.handlers = []
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
+    handlers=handlers
 )
